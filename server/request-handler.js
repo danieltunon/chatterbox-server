9904f410eb7c8c2c57525c8099b94555192ab531
swfree <skye.free@gmail.com>
2016-03-21 19:37:23 -0700

    Complete bare requirements and then some

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,69 +1,143 @@
-/*************************************************************
-
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
+var fs = require('fs');
+var path = require('path');
 
 var requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
 
   // The outgoing status.
   var statusCode = 200;
-
-  // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
+  var dirname = '/Users/student/Codes/2016-02-chatterbox-server/client';
 
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
-  headers['Content-Type'] = 'text/plain';
+  // gross floating storage object
+  global.storage = global.storage || { results: [ { objectId: 1, username: 'danny', message: 'poo' }, { objectId: 2, username: 'skye', message: 'hai' } ] };
 
-  // .writeHead() writes to the request line and headers of the response,
-  // which includes the status and all headers.
-  response.writeHead(statusCode, headers);
+  if (request.method === 'GET') {
+    if (request.url === '/' || /\?username=.*/.test(request.url)) {
+      headers['Content-Type'] = 'text/html';
 
-  // Make sure to always call response.end() - Node may not send
-  // anything back to the client until you do. The string you pass to
-  // response.end() will be the body of the response - i.e. what shows
-  // up in the browser.
-  //
-  // Calling .end "flushes" the response's internal buffer, forcing
-  // node to actually send all the data over to the client.
-  response.end('Hello, World!');
+      fs.readFile('/Users/student/Codes/2016-02-chatterbox-server/client/refactor.html', 'UTF-8', function(err, data) {
+        if (err) {
+          throw err;
+        }
+        console.log(data);
+        response.writeHead(statusCode, headers);
+        response.end(data);
+      });
+
+// Serving request type GET for url /styles/styles.css
+// Serving request type GET for url /bower_components/jquery/dist/jquery.js
+// Serving request type GET for url /bower_components/underscore/underscore.js
+// Serving request type GET for url /bower_components/backbone/backbone.js
+// Serving request type GET for url /env/config.example.js
+// Serving request type GET for url /scripts/refactor.js
+// Serving request type GET for url /images/spiffygif_46x46.gif
+
+
+
+
+    } else if (/css$/.test(request.url)) {
+
+      headers['Content-Type'] = 'text/css';
+      response.writeHead(statusCode, headers);
+      fs.createReadStream(dirname + request.url, 'UTF-8').pipe(response);
+
+    } else if (/jquery\.js$/.test(request.url)) { 
+      headers['Content-Type'] = 'text/javascript';
+      response.writeHead(statusCode, headers);
+      var stream = fs.createReadStream(dirname + request.url, 'UTF-8');
+      stream.pipe(response);
+
+    } else if (/underscore\.js$/.test(request.url)) { 
+      headers['Content-Type'] = 'text/javascript';
+      response.writeHead(statusCode, headers);
+      var stream = fs.createReadStream(dirname + request.url, 'UTF-8');
+      stream.pipe(response);
+
+    } else if (/example\.js$/.test(request.url)) { 
+      headers['Content-Type'] = 'text/javascript';
+      response.writeHead(statusCode, headers);
+      var stream = fs.createReadStream(dirname + request.url, 'UTF-8');
+      stream.pipe(response);
+    
+    } else if (/backbone\.js$/.test(request.url)) { 
+      headers['Content-Type'] = 'text/javascript';
+      response.writeHead(statusCode, headers);
+      var stream = fs.createReadStream(dirname + request.url, 'UTF-8');
+      stream.pipe(response);
+
+    } else if (/refactor\.js$/.test(request.url)) {   
+      headers['Content-Type'] = 'text/javascript';
+      response.writeHead(statusCode, headers);
+      var stream = fs.createReadStream(dirname + request.url, 'UTF-8');
+      stream.pipe(response);
+
+    } else if (/46\.gif$/.test(request.url)) {   
+      headers['Content-Type'] = 'image/gif';
+      response.writeHead(statusCode, headers);
+      var stream = fs.createReadStream(dirname + request.url);
+      stream.pipe(response);
+
+    } else if (request.url === '/classes/messages') {
+      console.log('...getting messages');
+
+      headers['Content-Type'] = 'text/json';
+      response.writeHead(statusCode, headers);
+      response.end(JSON.stringify(storage));
+
+    } else if (request.url === '/classes/rooms') {
+      console.log('...getting rooms');
+
+      headers['Content-Type'] = 'text/json';
+      response.writeHead(statusCode, headers);
+      response.end(someData);
+    } else {
+      headers['Content-Type'] = 'text/plain';
+      response.writeHead(404, headers);
+      response.end('Page not found');
+    }
+  } else if (request.method === 'POST') {
+    if (request.url === '/classes/messages') {
+      console.log('...posting your message');
+      headers['Content-Type'] = 'text/json';
+      response.writeHead(201, headers);
+
+      var incomingMessage = '';
+      request.on('data', function(chunk) {
+        incomingMessage += chunk;
+      });
+
+      request.on('end', function() {
+        storage.results.unshift(JSON.parse(incomingMessage));
+        console.log(JSON.stringify(storage));
+        response.end(JSON.stringify(storage));
+      });
+
+    } else if (request.url === '/classes/rooms') {
+      console.log('...posting a room');
+
+      headers['Content-Type'] = 'text/json';
+      response.writeHead(statusCode, headers);
+      // response will be entire storage object
+      response.end(someData);
+    } else {
+      headers['Content-Type'] = 'text/plain';
+      response.writeHead(404, headers);
+      response.end('Page not found');
+    }
+  } else if (request.method === 'OPTIONS') {
+    // OPTIONS response
+
+  } else {
+    // error
+    response.writeHead(404, headers);
+    response.end('Bad server request');
+  }
+
+
 };
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -71,3 +145,4 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
+module.exports = requestHandler;
